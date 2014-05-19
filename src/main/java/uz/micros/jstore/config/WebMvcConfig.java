3278cc77bd883ea029718feaf2a7a354f7da6e6f
voris90 <voris90@yahoo.com>
2014-05-19 20:05:01 +0500

    JQUERY, JAVASCRIPT, CSS new to jstore

--- src/main/java/uz/micros/jstore/config/WebMvcConfig.java
+++ src/main/java/uz/micros/jstore/config/WebMvcConfig.java
@@ -3,7 +3,9 @@ package uz.micros.jstore.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by java on 12.05.14.
@@ -12,5 +14,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 @EnableWebMvc
 @Configuration
 @ComponentScan
-public class WebMvcConfig extends WebMvcConfigurationSupport{
+public class WebMvcConfig extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resource/webjars/");
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
 }
