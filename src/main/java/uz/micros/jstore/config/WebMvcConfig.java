70e7975a88c3412401f980c16be65fbab4bc0dd0
voris90 <voris90@yahoo.com>
2014-06-02 20:03:04 +0500

    Interceptor (JSTORE)

--- src/main/java/uz/micros/jstore/config/WebMvcConfig.java
+++ src/main/java/uz/micros/jstore/config/WebMvcConfig.java
@@ -2,10 +2,8 @@ package uz.micros.jstore.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
+import uz.micros.jstore.util.Interceptor;
 
 /**
  * Created by java on 12.05.14.
@@ -20,5 +18,10 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter{
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new Interceptor());
     }
 }
